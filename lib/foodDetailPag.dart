17e7c79da93e61048a2f16e548c04c5c82651a17
abdijabaar <abdijabaar443@gmail.com>
2024-11-07 03:59:23 -0800

    initial commit

---
+++ lib/foodDetailPag.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main(){
+  runApp(app)
+}
+class FoodApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'FoodGo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget{
+  @override
+  _SplashScreenState createState() => _SplshScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>{
+  @override
+  void initStat() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      ),
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Text(
+          "FoodGo",
+          style: TextStyle(
+            fontSize: 48,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'foodDetailPag.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Map<String , dynamic>> foodItems = [
+    {'name': 'cheeseburger', 'image': 'purger1.jpg', 'price': 3},
+    {'name': 'burger', 'image': 'purger2.jpg', 'price': 4}
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FoodGo'),
+      ),
+      body: GridView.builder(
+        itemCount: foodItems.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (Context ,index) {
+          final item = foodItems[index];
+          return GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => FoodDetailPage(
+                    foodName: item['name'],
+
+                  )
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+}
